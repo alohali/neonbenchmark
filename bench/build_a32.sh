fb0a2b45808b7f14bd64e96f36ad6d5a49790af8
dtcrong <yanrongli@outlook.com>
2018-08-30 01:48:39 +0000

    build a32,a64 OK

---
+++ bench/build_a32.sh
@@ -0,0 +1,2 @@
+#g++ benchmark_memory.cpp -fopenmp -o a.out -g -ggdb -O0 -std=c++11 -marm -mfpu=neon -std=c++11 -mfloat-abi=softfp -static
+g++ benchmark_memory.cpp -fopenmp -ofast -o a.out -std=c++11  -std=c++11 -marm -mfpu=neon
